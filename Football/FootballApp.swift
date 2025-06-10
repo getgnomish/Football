b3140a227d2a85948d5c65e4b8014931fd727e38
Derek Meyers <p.derekmeyers@gmail.com>
2025-06-10 05:49:47 -0400

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Derek Meyers on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
